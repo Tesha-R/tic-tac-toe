330afd5cb0200ff8edd50bca65c16396c95b1df7
Tesha R <tesha@ldrdesigns.com>
2022-08-10 17:29:37 -0700

    Updated game logic for evaluating winner

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 const app = document.getElementById("app");
 
 let gameData = {
-  gameBoard: ["", "", "", "", "", "", "", "", ""],
-  player1: {
-    name: "Player 1",
-    gamePiece: "X",
-    turn: true,
-    player1Game: "",
-    score: 0
-  },
-  player2: {
-    name: "Player 2",
-    gamePiece: "O",
-    turn: false,
-    player2Game: "",
-    score: 0
-  },
-  winning: ["012", "345", "678", "036", "147", "258", "048", "246"],
-  messages: {
-    player1wins: "Player 1 wins!",
-    player2wins: "Player 2 wins!",
-    gameIsATie: "Game is a tie!"
-  }
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+    player1: {
+        name: "Player 1",
+        gamePiece: "X",
+        turn: true,
+        score: 0
+    },
+    player2: {
+        name: "Player 2",
+        gamePiece: "O",
+        turn: false,
+        score: 0
+    },
+    winningCombos: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ],
+    messages: {
+        player1wins: "Player 1 wins!",
+        player2wins: "Player 2 wins!",
+        gameIsATie: "Game is a tie!"
+    }
 };
-// X X X  > 012
-// X X X  > 345
-// X X X  > 678
+// 0 1 2
+// 3 4 5
+// 6 7 8
 
 // Select opponent player or computer 
 // Start game
@@ -38,81 +45,102 @@ function gameMessage(message) {
     <button class="new-game-btn">New game</button>
   </div>
   `;
-  app.innerHTML += html;
+    app.innerHTML += html;
 }
 
-// Check for winners
 function checkForWinner() {
-    const player1Winner = gameData.player1.player1Game.split("").sort().join("");
-    const player2Winner = gameData.player2.player2Game.split("").sort().join("");
-  if (gameData.winning.includes(player1Winner)) {
-    gameMessage(gameData.messages.player1wins)
-    console.log("Player 1 wins");
-  } else if (gameData.winning.includes(player2Winner)) {
-    console.log("Player 2 wins");
-    gameMessage(gameData.messages.player2wins)
-   }
-//    else {
-//     gameMessage(gameData.messages.gameIsATie)
-//   }
+    const {
+        winningCombos,
+        gameBoard
+    } = gameData
+    // iterate through array 7 indexes
+    for (let i = 0; i < winningCombos.length; i++) {
+        //console.log(winningCombos[i]);
+        const winningCombo = winningCombos[i];
+        // save each column of numbers to a variable, indexes 0,1,2
+        let a = gameBoard[winningCombo[0]]
+        // console.log(a);
+        let b = gameBoard[winningCombo[1]]
+        // console.log(b);
+        let c = gameBoard[winningCombo[2]]
+        // console.log(c);
+        if (a === '' || b === '' || c === '') {
+            continue
+        }
+        if (a === b && b === c) {
+            console.log("winner", "abc", a, b, c, )
+            if (a === 'X' && b === 'X' && c === 'X') {
+                return gameMessage(gameData.messages.player1wins)
+            } else if (a === 'O' && b === 'O' && c === 'O') {
+                return gameMessage(gameData.messages.player2wins)
+            }
+            // determine a tie
+
+        } else {
+            console.log("not winner", a, b, c)
+        }
+    }
 }
 
 // new game
 function startGame() {
-  gameData.gameBoard = ["", "", "", "", "", "", "", "", ""];
-  gameData.player1.player1Game = "";
-  gameData.player2.player2Game = "";
-  gameData.player1.score = 0;
-  gameData.player2.score = 0;
-  renderGame(gameData.gameBoard);
+    gameData.gameBoard = ["", "", "", "", "", "", "", "", ""];
+    gameData.player1.player1Game = "";
+    gameData.player2.player2Game = "";
+    gameData.player1.score = 0;
+    gameData.player2.score = 0;
+    renderGame(gameData.gameBoard);
 }
 
 // render the contents of the gameboard array
 function renderGame(dataArr) {
-  let html = "";
-  dataArr.forEach((item, index) => {
-    html += `<div class="game-square" data-id="${index}">${item}</div>`;
-    return (app.innerHTML = `<div class="game-board"><h1 class="game-title">Tic Tac Toe</h1>${html} </div>`);
-  });
+    let html = "";
+    dataArr.forEach((item, index) => {
+        html += `<div class="game-square" data-id="${index}">${item}</div>`;
+        return (app.innerHTML = `<div class="game-board"><h1 class="game-title">Tic Tac Toe</h1>${html} </div>`);
+    });
 }
 
 // event listeners
 
 app.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (e.target.matches(".new-game-btn")) {
-    console.log("new game button clicked");
-    startGame();
-  }
+    e.preventDefault();
+    if (e.target.matches(".new-game-btn")) {
+        console.log("new game button clicked");
+        startGame();
+    }
 });
 
+
+
 app.addEventListener("click", (e) => {
-  // get the current square clicked
-  let currSquare = e.target.dataset.id;
-  if (e.target.matches(".game-square") && gameData.player1.turn === true) {
-    // add game piece to corresponding array index
-    gameData.gameBoard.splice(currSquare, 1, gameData.player1.gamePiece);
-    gameData.player1.player1Game += currSquare;
-    //console.log("player1Game", gameData.player1.player1Game);
-    renderGame(gameData.gameBoard);
-    gameData.player1.turn = false;
-    gameData.player2.turn = true;
-    checkForWinner();
-  } else if (
-    e.target.matches(".game-square") &&
-    gameData.player2.turn === true
-  ) {
-    gameData.gameBoard.splice(currSquare, 1, gameData.player2.gamePiece);
-    renderGame(gameData.gameBoard);
-    gameData.player2.player2Game += currSquare;
-    //console.log("playerGame2", gameData.player2.player2Game);
-    gameData.player2.turn = false;
-    gameData.player1.turn = true;
-    checkForWinner();
-  }
+    // get the current square clicked
+    let currSquare = e.target.dataset.id;
+    if (e.target.matches(".game-square") && gameData.player1.turn === true) {
+        // add game piece to corresponding array index
+        gameData.gameBoard.splice(currSquare, 1, gameData.player1.gamePiece);
+        renderGame(gameData.gameBoard);
+        console.log(gameData.gameBoard)
+        gameData.player1.turn = false;
+        gameData.player2.turn = true;
+        checkForWinner();
+    } else if (
+        e.target.matches(".game-square") &&
+        gameData.player2.turn === true
+    ) {
+        gameData.gameBoard.splice(currSquare, 1, gameData.player2.gamePiece);
+        renderGame(gameData.gameBoard);
+        console.log(gameData.gameBoard)
+        //  gameData.player2.player2Game += currSquare;
+        console.log("playerGame2", gameData.player2.player2Game);
+        gameData.player2.turn = false;
+        gameData.player1.turn = true;
+        checkForWinner();
+    }
 });
+
 function render() {
-  renderGame(gameData.gameBoard);
+    renderGame(gameData.gameBoard);
 }
 
 render();
